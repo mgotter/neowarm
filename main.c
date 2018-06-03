515e385cd30813a16da135abc45c34ccc22de9e3
mgotter <mgotter@gmail.com>
2018-06-03 17:26:43 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,31 @@
+/* main.c */
+
+/* Includes */
+#include <stdint.h>
+
+/* POSIX Header Files */
+#include <pthread.h>
+
+/* RTOS Header Files */
+#include <FreeRTOS.h>
+#include <task.h>
+
+/* Driver Header Files */
+#include <ti/drivers/GPIO.h>
+
+/* Board Header Files */
+#include "Board.h"
+
+/* Local Header Files */
+//#include "callbacks.h"
+//#include "uart_thread.h"
+//#include "led_thread.h"
+
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+int main(void)
+{
+
+
+    return (0);
+}
